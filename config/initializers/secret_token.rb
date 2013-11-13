c24436952b149522f490264c3134eef392792e78
Christopher Haupt <chaupt@mobirobo.com>
2013-11-13 13:04:03 -0800

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Findmybike::Application.config.secret_key_base = 'f4903fad6fae0265e3d593c63429d6c0ec298167f6ae5edb182ef9c6fbb40ee7fc8bc27bd6799005202878c74dd36199ca769c66da064a9ddfffef18fbc2f77d'
